eaf3674a5f3e2507de6e3834e9aa7002a191d370
Fan Yang <yangfan879@gmail.com>
2015-10-23 16:01:09 +0800

    initial commit

---
+++ file/file.c
@@ -0,0 +1,26 @@
+#define _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t readat(int fd, void *buf, size_t count, off_t offset)
+{
+	return pread(fd, buf, count, offset);
+}
+
+ssize_t writeat(int fd, void *buf, size_t count, off_t offset)
+{
+	return pwrite(fd, buf, count, offset);
+}
+
+int alloc(int fd, off_t offset, off_t len)
+{
+	return fallocate(fd, 0, offset, len);
+}
+
+int dealloc(int fd, off_t offset, off_t len)
+{
+	return fallocate(fd, FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE,
+			 offset, len);
+}
